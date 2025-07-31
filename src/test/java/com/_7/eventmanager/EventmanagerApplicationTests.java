d14d78004f92fd17074bf789f25093c35034a117
Rim Ouarrak <rimouarrak2002.com>
2025-07-31 12:35:12 +0100

    init

---
+++ src/test/java/com/_7/eventmanager/EventmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com._7.eventmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
